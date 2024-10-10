564013f26f2a7a2834fd5009b6ae8b7a86e39c2d
dheeraj-thakur123 <dheerajt15@gmail.com>
2024-10-10 23:00:47 +0530

    Test Case 2 3 4

--- src/App.test.js
+++ src/App.test.js
@@ -7,25 +7,70 @@ import App from './App';
 describe('App Component', () => {
   test('base case displays 0 when the input is empty and the add button is clicked', () => {
       render(<App />);
-
       // Get the input and button elements
       const input = screen.getByRole('textbox'); 
       const button = screen.getByRole('button', { name: /add/i });
-
       // click event
       fireEvent.click(button);
-
       // result display 0
       expect(screen.getByText(/0/i)).toBeInTheDocument();
   });
 
-  test('calculates the sum when user enters "1,2,3" and clicks add', () => {
+  test('case-1 the sum when user enters "1,2,3" and clicks add', () => {
     render(<App />);
     const input = screen.getByRole('textbox');
     const button = screen.getByRole('button', { name: /add/i });
     fireEvent.change(input, { target: { value: '1,2,3' } });
     fireEvent.click(button);
     expect(screen.getByText(/6/i)).toBeInTheDocument();
+});
+
+test('case-2 displays error message when user enters negative number', () => {
+  render(<App />);
+  
+  const input = screen.getByRole('textbox');
+  const button = screen.getByRole('button', { name: /add/i });
+
+  fireEvent.change(input, { target: { value: '1,-2,3' } });
+  fireEvent.click(button);
+
+  expect(screen.getByText(/Invalid input: -2/i)).toBeInTheDocument();
+});
+
+test('case-3 displays error message when user enters all negative numbers', () => {
+  render(<App />);
+  
+  const input = screen.getByRole('textbox');
+  const button = screen.getByRole('button', { name: /add/i });
+
+  fireEvent.change(input, { target: { value: '-1,-2,-3' } });
+  fireEvent.click(button);
+
+  expect(screen.getByText(/Invalid input: -1, -2, -3/i)).toBeInTheDocument();
+});
+
+test('case-4 displays error message when user enters characters', () => {
+  render(<App />);
+  
+  const input = screen.getByRole('textbox');
+  const button = screen.getByRole('button', { name: /add/i });
+
+  fireEvent.change(input, { target: { value: '1,2,a' } });
+  fireEvent.click(button);
+
+  expect(screen.getByText(/Invalid input: a/i)).toBeInTheDocument();
+});
+
+test('case-5 displays error message when user enters all chracters', () => {
+  render(<App />);
+  
+  const input = screen.getByRole('textbox');
+  const button = screen.getByRole('button', { name: /add/i });
+
+  fireEvent.change(input, { target: { value: 'a,b,c' } });
+  fireEvent.click(button);
+
+  expect(screen.getByText(/Invalid input: a, b, c/i)).toBeInTheDocument();
 });
 
 
--- src/component/stringCalc.js
+++ src/component/stringCalc.js
@@ -8,12 +8,31 @@ const StringCalc = () => {
 
   const handleAdd = ()=>{
     //base case user input is empty
+     setUserInput('');
+     setResult('');
+     setError('')
     if(userInput==''){
       setResult(0)
     }else{
-        const numbers = userInput.split(',').map(Number);
-        const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-        setResult(sum);
+        
+         // Split the input by new lines or commas
+    const numbers = userInput.split(/[\n,]+/).map(str => str.trim());
+
+    // Check for non-numeric inputs and negative numbers
+    const invalidChars = numbers.filter(num => num !== '' && isNaN(Number(num)));
+    const negativeNumbers = numbers.filter(num => num !== '' && Number(num) < 0);
+        if(invalidChars.length>0){
+            setUserInput('');
+            setResult('');
+            setError(`Invalid input: ${invalidChars.join(', ')}`);
+        }else if(negativeNumbers.length>0){
+            setUserInput('');
+            setResult('');
+            setError(`Invalid input: ${negativeNumbers.join(', ')}`);
+        }else{
+            const sum = numbers.reduce((acc, curr) => acc + Number(curr), 0).toString();;
+            setResult(sum);
+        }
 
     }
   }
@@ -21,17 +40,15 @@ const StringCalc = () => {
   return (
      <div className='calc_box'>
      <h2 className='calc_heading'>String - Calculator</h2>
-      <div className='calc_input'>
         <div className='calc_result'>{result}</div>
         <hr/>
-       <div>
+       <div className='input_container'>
         <input className='input_box' onChange = {(e)=>{setUserInput(e.target.value)}} type='text' value={userInput}/>
         {error && <span className='input_err'>{error}</span>}
        </div>
        <div>
         <button className='add_btn' onClick={handleAdd}>add</button>
        </div>
-      </div>
      </div>
   );
 }
